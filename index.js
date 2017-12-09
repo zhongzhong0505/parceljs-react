dc7a2d6b7f858acba0fe589219fa81fabb199410
zhongzhong0505 <zt_zhong@qq.com>
2017-12-09 12:11:55 +0800

    把路由添加进来了

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM  from "react-dom";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OneComponent from './src/components/OneComponent';
+import TwoComponent from './src/components/TwoComponent';
+import ThreeComponent from './src/components/ThreeComponent';
 
 class App extends React.Component {
   render() {
-    return <div>Hello Parceljs!</div>
+
+    return <div>
+      <Router>
+        <div>
+          <Link to={'/one'} style={{marginRight: 20}}>One</Link>
+          <Link to={'/two'} style={{marginRight: 20}}>Two</Link>
+          <Link to={'/three'} style={{marginRight: 20}}>Three</Link>
+          <Route path="/one" component={OneComponent}></Route>
+          <Route path="/two" component={TwoComponent}></Route>
+          <Route path="/three" component={ThreeComponent}></Route>
+        </div>
+      </Router>
+    </div>
   }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"))
+
+if (module.hot) {
+  module.hot.accept();
+}
 
---
+++ src/components/OneComponent.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+export default class extends Component{
+  render(){
+    return <div>one component</div>
+  }
+}
