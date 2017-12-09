6f9f54e431957ff9b211935770e079322c9a218e
zhongzhong0505 <zt_zhong@qq.com>
2017-12-09 11:20:52 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM  from "react-dom";
+
+const App =() => {
+    return <div>Hello Parceljs!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
+
